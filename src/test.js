fd2a48a6884067e2046377dfe104822616c58417
JiangZhu <zhu.jiang@56qq.com>
2018-06-21 19:09:56 +0800

    feat: 增加测试用例

--- src/test.js
+++ src/test.js
@@ -1,7 +1,35 @@
-import ExampleComponent from './'
+import React from 'react';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import Case from './Case';
+import Default from './Default';
 
-describe('ExampleComponent', () => {
-  it('is truthy', () => {
-    expect(ExampleComponent).toBeTruthy()
-  })
-})
+configure({ adapter: new Adapter() });
+
+describe('Case render', () => {
+  it('render empty component', () => {
+    const cc = shallow(<Case></Case>);
+    expect(cc.type()).toEqual(null);
+  });
+  it('render non-null component', () => {
+    const SubComponent = <div>Case render</div>;
+    const cc = shallow(<Case>{SubComponent}</Case>);
+    expect(cc.type()).toEqual('div');
+  });
+});
+describe('Default render', () => {
+  it('render empty component', () => {
+    const cc = shallow(<Default></Default>);
+    expect(cc.type()).toEqual(null);
+  });
+  it('render non-null component', () => {
+    const SubComponent = <div>Default render</div>;
+    const cc = shallow(<Default>{SubComponent}</Default>);
+    expect(cc.type()).toEqual('div');
+  });
+});
+
+
+function log() {
+  console.log.apply(this, arguments);
+}
