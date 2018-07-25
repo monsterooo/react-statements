3a73e219eec91ac21f791443e51de92fa70a204c
JiangZhu <zhu.jiang@56qq.com>
2018-07-25 15:54:53 +0800

    feat: 为If组件增加Default支持

--- src/If.js
+++ src/If.js
@@ -1,8 +1,23 @@
+import React from 'react';
 import PropTypes from 'prop-types';
+import Default from './Default';
 
 const If = ({ when, children }) => {
+  let defaultComponent = null;
+  let ifComponent = null;
+
+  React.Children.forEach(children, (child) => {
+    if (child.type === Default) {
+      defaultComponent = child;
+    } else {
+      ifComponent = child;
+    }
+  });
+
   if (when) {
-    return children;
+    return ifComponent;
+  } else if (defaultComponent) {
+    return defaultComponent;
   }
   return null;
 };
--- src/test.js
+++ src/test.js
@@ -3,9 +3,36 @@ import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import Case from './Case';
 import Default from './Default';
+import If from './If';
 
 configure({ adapter: new Adapter() });
 
+describe('If render', () => {
+  it('If when value is true', () => {
+    const name = 'react-statement';
+    const p = <p>{name}</p>;
+    const renderIf = shallow(<If when={name}>{p}</If>);
+    expect(renderIf.equals(p)).toEqual(true);
+  });
+  it('If when value is false', () => {
+    const name = '';
+    const p = <p>{name}</p>;
+    const renderIf = shallow(<If when={name}>{p}</If>);
+    expect(renderIf.children().length).toEqual(0);
+  });
+  it('If when value is Default', () => {
+    const name = '';
+    const p = <p>{name}</p>;
+    const RenderDefault = <div>render else</div>
+    const renderIf = shallow(
+      <If when={name}>
+        {p}
+        <Default children={RenderDefault} />
+      </If>
+    );
+    expect(renderIf.children().equals(RenderDefault)).toEqual(true);
+  });
+})
 describe('Case render', () => {
   it('render empty component', () => {
     const cc = shallow(<Case></Case>);
